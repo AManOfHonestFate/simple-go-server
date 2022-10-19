f2c9be1d022dec1b7381772aecc0b544e51eb953
Egor <khromov.developer@mail.ru>
2022-10-19 15:54:40 +0300

    using html template now

--- wiki.go
+++ wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 )
@@ -25,14 +25,28 @@ func loadPage(title string) (*Page, error) {
 	return &Page{title, body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	page, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h2>\n<p>%s</p>", page.Title, page.Body)
-	return
+	renderTemplate(w, "view", page)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	page, err := loadPage(title)
+	if err != nil {
+		return
+	}
+	renderTemplate(w, "edit", page)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	http.ListenAndServe(":8080", nil)
 }
