6abacc8414e8bc23bcde6fd11f47ce69276de543
Egor <khromov.developer@mail.ru>
2022-10-19 16:04:02 +0300

    refactor: added comments and var renames

--- wiki.go
+++ wiki.go
@@ -11,11 +11,13 @@ type Page struct {
 	Body  []byte
 }
 
+// Save page locally
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
+// Load page from local directory
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename)
@@ -25,22 +27,28 @@ func loadPage(title string) (*Page, error) {
 	return &Page{title, body}, nil
 }
 
+// Render html template
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
+	temp, _ := template.ParseFiles(tmpl + ".html")
+	err := temp.Execute(w, p)
+	if err != nil {
+		w.Write([]byte("Can't render html"))
+	}
 }
 
+// handles /view request
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	page, _ := loadPage(title)
 	renderTemplate(w, "view", page)
 }
 
+// handles /edit request
 func editHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/edit/"):]
 	page, err := loadPage(title)
 	if err != nil {
-		return
+		page = &Page{Title: "Edit"}
 	}
 	renderTemplate(w, "edit", page)
 }
